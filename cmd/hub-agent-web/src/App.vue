5a693f6e37258fd8538bc8855e0120d016c9c429
Firas Qutishat <firas.qutishat@securekey.com>
2019-11-29 15:27:59 -0500

    feat: Create hub agent web
    closes #1
    
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

---
+++ cmd/hub-agent-web/src/App.vue
@@ -0,0 +1,20 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+<template>
+  <div id="app">
+    <nav>
+    <router-link :to="{ name: 'createInvitation' }">createInvitation</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App"
+  };
+</script>
---
+++ cmd/hub-agent-web/src/router/index.js
@@ -0,0 +1,20 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            name: "createInvitation",
+            path: "/CreateInvitation",
+            component: () => import("@/views/CreateInvitation")
+        }
+    ]
+});
